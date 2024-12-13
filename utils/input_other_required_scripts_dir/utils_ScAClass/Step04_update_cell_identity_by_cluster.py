5fa9a2933f523dc9eedcafb25b8858337e841aec
yanhaidong1 <35510112+yanhaidong1@users.noreply.github.com>
2024-12-13 11:47:29 +0800

    Add files via upload

--- utils/input_other_required_scripts_dir/utils_ScAClass/Step04_update_cell_identity_by_cluster.py
+++ utils/input_other_required_scripts_dir/utils_ScAClass/Step04_update_cell_identity_by_cluster.py
@@ -16,17 +16,20 @@ def update_prediction_by_cluster (input_clustering_fl,input_cell_prediction_fl,i
 
     ##eachPC + '\t' + eachmethod + '\t' + eachcell + '\t' + final_annot
     store_cluster_cell_list_dic = {}
+    count = 0
     with open (input_clustering_fl,'r') as ipt:
         for eachline in ipt:
             eachline = eachline.strip('\n')
             col = eachline.strip().split()
             cellnm = col[0]
-            cluster = col[1]
-            if cluster in store_cluster_cell_list_dic:
-                store_cluster_cell_list_dic[cluster].append(cellnm)
-            else:
-                store_cluster_cell_list_dic[cluster] = []
-                store_cluster_cell_list_dic[cluster].append(cellnm)
+            cluster = col[-1]
+            count += 1
+            if count != 1:
+                if cluster in store_cluster_cell_list_dic:
+                    store_cluster_cell_list_dic[cluster].append(cellnm)
+                else:
+                    store_cluster_cell_list_dic[cluster] = []
+                    store_cluster_cell_list_dic[cluster].append(cellnm)
 
     store_cell_annot_dic = {}
     with open (input_cell_prediction_fl,'r') as ipt:
@@ -81,6 +84,56 @@ def update_prediction_by_cluster (input_clustering_fl,input_cell_prediction_fl,i
         for eachline in store_final_line_eachcell_list:
             opt.write(eachline + '\n')
 
+    ##updating 121324 we will update the cell identity in the original cluster file
+    store_cell_annot_not_by_cluster_dic = {}
+    count = 0
+    with open (input_output_dir + '/opt_final_cell_annotation.txt','r') as ipt:
+        for eachline in ipt:
+            eachline = eachline.strip('\n')
+            col = eachline.strip().split()
+            count += 1
+            if count != 1:
+                cellID = col[2]
+                annot = col[3]
+                store_cell_annot_not_by_cluster_dic[cellID] = annot
+
+    store_cell_annot_by_cluster_dic = {}
+    with open (input_output_dir + '/opt_final_cell_annotation_per_cluster.txt','r') as ipt:
+        for eachline in ipt:
+            eachline = eachline.strip('\n')
+            col = eachline.strip().split()
+            cellID = col[1]
+            annot = col[2]
+            store_cell_annot_by_cluster_dic[cellID] = annot
+
+    store_final_line_list = []
+    count = 0
+    with open (input_clustering_fl,'r') as ipt:
+        for eachline in ipt:
+            eachline = eachline.strip('\n')
+            col = eachline.strip().split()
+            count += 1
+            if count != 1:
+                cellID = col[0]
+                if cellID in store_cell_annot_not_by_cluster_dic:
+                    ScAClass = store_cell_annot_not_by_cluster_dic[cellID]
+                else:
+                    ScAClass = 'Unknown'
+
+                if cellID in store_cell_annot_by_cluster_dic:
+                    ScAClass_clust = store_cell_annot_by_cluster_dic[cellID]
+                else:
+                    ScAClass_clust = 'Unknown'
+
+                final_line = eachline + '\t' + ScAClass + '\t' + ScAClass_clust
+                store_final_line_list.append(final_line)
+            else:
+                final_line = eachline + '\t' + 'ScAClass' + '\t' + 'ScAClass_clust'
+                store_final_line_list.append(final_line)
+
+    with open (input_output_dir + '/opt_meta_ScAClass_predicted_cell_identity.txt','w+') as opt:
+        for eachline in store_final_line_list:
+            opt.write(eachline + '\n')
 
 update_prediction_by_cluster (input_clustering_fl,input_cell_prediction_fl,input_output_dir)
 
