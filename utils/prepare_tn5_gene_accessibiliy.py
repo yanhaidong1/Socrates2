b58b8fb459fa185ac499296605286dad52a9c99b
yanhaidong1 <35510112+yanhaidong1@users.noreply.github.com>
2025-01-04 17:33:32 +0800

    Add files via upload

--- utils/prepare_tn5_gene_accessibiliy.py
+++ utils/prepare_tn5_gene_accessibiliy.py
@@ -4,6 +4,8 @@ import argparse
 import glob
 import sys
 import os
+import re
+import subprocess
 
 
 ##this script is to process data bam file from cellranger
@@ -26,32 +28,37 @@ def get_parsed_args():
     parser.add_argument("-BAM_fl", dest='bam_file', help="Provide bam file obtained from cell ranger")
 
     ##step 02
-    parser.add_argument("-Ggff_fl", dest='gene_gff_file',
-                        help='Provide the gene gff file used to extract exon region when permutating peaks.')
+    ##updating 010425
+    parser.add_argument("-soc_obj", dest = 'soc_object_fl', help ='Provide an object obtained from the clustering step.')
 
-    parser.add_argument("-Gmfai_fl", dest='genome_fai_file', help='Provide a fai index file of reference genome.')
+
+    parser.add_argument("-Ggff_fl", dest='gene_gff_file', help='Provide the gene gff file used to extract exon region when permutating peaks.')
+
+    #parser.add_argument("-Gmfai_fl", dest='genome_fai_file', help='Provide a fai index file of reference genome.')
 
     parser.add_argument("-Genome_fl", dest = 'genome_fasta_file', help='Povide a genome fasta file.')
 
     ##output from the step 01
     parser.add_argument("-tn5_fl", dest = 'tn5_file', help = 'Provide a tn5 insertion file obtained from the -open_procBAM step.')
 
+
     ##step 03
-    parser.add_argument("-meta_fl", dest='meta_file', help='Provide a meta file aftering the clustering')
+    ##Users could directly use the soc object generated from the last step
+    #parser.add_argument("-meta_fl", dest='meta_file', help='Provide a meta file aftering the clustering')
 
     ##output from the step 02
-    parser.add_argument("-gene_tn5_fl", dest='gene_tn5_file', help = 'Provide a gene tn5 file obtained from the -open_geneTn5 step.')
+    #parser.add_argument("-gene_tn5_fl", dest='gene_tn5_file', help = 'Provide a gene tn5 file obtained from the -open_geneTn5 step.')
 
-    parser.add_argument("-gene_bed_fl", dest = 'gene_bed_file', help = 'Provide a gene bed file obtained from the -open_geneTn5 step.')
+    #parser.add_argument("-gene_bed_fl", dest = 'gene_bed_file', help = 'Provide a gene bed file obtained from the -open_geneTn5 step.')
 
     ##step 04
-    parser.add_argument("-svd_fl", dest = 'svd_file', help='Provide a svd file aftering the clusteirng')
+    #parser.add_argument("-svd_fl", dest = 'svd_file', help='Provide a svd file aftering the clusteirng')
 
     parser.add_argument("-clustnm", dest = 'target_cluster_nm', help = 'Provide a target cluster name in the meta file to smooth the accessibility.'
                                                                        'Default: LouvainClusters')
 
     ##output from the step03
-    parser.add_argument("-gene_acc_fl", dest = 'gene_accessibility_file', help = 'Provide a gene accessibility file obtained from the -open_geneAcc step.')
+    #parser.add_argument("-gene_acc_fl", dest = 'gene_accessibility_file', help = 'Provide a gene accessibility file obtained from the -open_geneAcc step.')
 
 
 
@@ -133,11 +140,22 @@ def main(argv=None):
                     print('There was an error opening the tn5 file!')
                     return
 
-
         else:
             s1_open_process_BAM_final = 'no'
             print('Users choose to close the BAM processing, please use \'-open_procBAM yes\' to open this step')
 
+
+    ###########################################################
+    ##this object should be provided in all the following steps
+    if args.soc_object_fl is None:
+        print('Cannot find the soc object file, please provide it')
+        return
+    else:
+        try:
+            file = open(args.soc_object_fl, 'r')  ##check if the file is not the right file
+        except IOError:
+            print('There was an error opening the soc object file!')
+            return
 
     ##step02
     if args.s2_open_prepare_gene_tn5 is None:
@@ -155,16 +173,6 @@ def main(argv=None):
                     file = open(args.gene_gff_file, 'r')  ##check if the file is not the right file
                 except IOError:
                     print('There was an error opening the gene gff file!')
-                    return
-
-            if args.genome_fai_file is None:
-                print('Cannot find genome fai index file, please provide it')
-                return
-            else:
-                try:
-                    file = open(args.genome_fai_file, 'r')  ##check if the file is not the right file
-                except IOError:
-                    print('There was an error opening the genome fai index file!')
                     return
 
             if args.genome_fasta_file is None:
@@ -189,6 +197,7 @@ def main(argv=None):
 
 
 
+
         else:
             s2_open_prepare_gene_tn5_final = 'no'
             print('Users choose to close the preparation of gene tn5 file, please use \'-open_geneTn5 yes\' to open this step')
@@ -200,40 +209,6 @@ def main(argv=None):
     else:
         if args.s3_open_gene_accessibility == 'yes':
             s3_open_gene_accessibility_final = 'yes'
-
-            ##input meta file
-            if args.meta_file is None:
-                print('Cannot find meta file, please provide it')
-                return
-            else:
-                try:
-                    file = open(args.meta_file, 'r')  ##check if the file is not the right file
-                except IOError:
-                    print('There was an error opening the meta file!')
-                    return
-
-            ##input gene tn5 sparse file
-            if args.gene_tn5_file is None:
-                print('Cannot find gene tn5 file, please provide it')
-                return
-            else:
-                try:
-                    file = open(args.gene_tn5_file, 'r')  ##check if the file is not the right file
-                except IOError:
-                    print('There was an error opening the gene tn5 file!')
-                    return
-
-            ##input gene bed file
-            if args.gene_bed_file is None:
-                print('Cannot find gene bed file, please provide it')
-                return
-            else:
-                try:
-                    file = open(args.gene_bed_file, 'r')  ##check if the file is not the right file
-                except IOError:
-                    print('There was an error opening the gene bed file!')
-                    return
-
         else:
             s3_open_gene_accessibility_final = 'no'
             print('Users choose to close the gene accessibility calculation, please use \'-open_geneAcc yes\' to open this step')
@@ -243,40 +218,6 @@ def main(argv=None):
     else:
         if args.s4_open_smooth_gene_accessibility == 'yes':
             s4_open_smooth_gene_accessibility_final = 'yes'
-
-            ##input meta file
-            if args.meta_file is None:
-                print('Cannot find meta file, please provide it')
-                return
-            else:
-                try:
-                    file = open(args.meta_file, 'r')  ##check if the file is not the right file
-                except IOError:
-                    print('There was an error opening the meta file!')
-                    return
-
-            ##input svd file
-            if args.svd_file is None:
-                print('Cannot find svd file, please provide it')
-                return
-            else:
-                try:
-                    file = open(args.svd_file, 'r')  ##check if the file is not the right file
-                except IOError:
-                    print('There was an error opening the svd file!')
-                    return
-
-            ##input gene accessibility file
-            if args.gene_accessibility_file is None:
-                print('Cannot find gene accessibility file, please provide it')
-                return
-            else:
-                try:
-                    file = open(args.gene_accessibility_file, 'r')  ##check if the file is not the right file
-                except IOError:
-                    print('There was an error opening the gene accessibility file!')
-                    return
-
         else:
             s4_open_smooth_gene_accessibility_final = 'no'
             print('Users choose to close the gene accessiblity smoothing, please use \'-smooth_geneAcc yes\' to open this step')
@@ -309,6 +250,8 @@ def main(argv=None):
         target_cluster_nm_final = 'LouvainClusters'
 
 
+
+
     if s1_open_process_BAM_final == 'yes':
 
         print('Users choose to run the step 01 to prepare process the bam file from cell ranger')
@@ -322,6 +265,18 @@ def main(argv=None):
         s1_procbam.process_bam (input_required_scripts_dir,input_bam_fl,s1_open_process_BAM_final_dir,
                                 core_number_run,mapping_quality_val,remove_step_s1_temp_final)
 
+    ##updating 010425 we will get the input prefix of the soc obj fl
+    input_soc_obj_fl = args.soc_object_fl
+    if re.match('.+/(.+)\.atac\.soc\.rds', input_soc_obj_fl):
+        mt = re.match('.+/(.+)\.atac\.soc\.rds', input_soc_obj_fl)
+        input_prefix = mt.group(1)
+    else:
+        if re.match('(.+)\.atac\.soc\.rds', input_soc_obj_fl):
+            mt = re.match('(.+)\.atac\.soc\.rds', input_soc_obj_fl)
+            input_prefix = mt.group(1)
+        else:
+            print('Please use *.atac.soc.rds file without changing the file name')
+            return
 
     if s2_open_prepare_gene_tn5_final == 'yes':
 
@@ -334,12 +289,23 @@ def main(argv=None):
         input_tn5_bed_fl = args.tn5_file
         #input_tn5_bed_fl = output_dir + '/open_process_BAM_final_dir/' + '/opt_tn5_mq' + mapping_quality_val + '.bed'
         input_gene_gff_fl = args.gene_gff_file
-        input_genome_fai_fl = args.genome_fai_file
+        #input_genome_fai_fl = args.genome_fai_file
         input_genome_fl = args.genome_fasta_file
 
+        ##build the fai index
+        ##updating 010425
+        cmd = 'samtools faidx ' + input_genome_fl
+        print(cmd)
+
+        input_genome_fai_fl = input_genome_fl + '.fai'
+
+
+        ##updating 010425
         s2_geneTn5.prepare_gene_tn5 (input_required_scripts_dir,input_tn5_bed_fl,s2_open_prepare_gene_tn5_final_dir,
                       input_gene_gff_fl,input_genome_fai_fl,
-                      input_genome_fl,final_category_gff)
+                      input_soc_obj_fl,
+                      input_prefix,
+                      final_category_gff)
 
     if s3_open_gene_accessibility_final == 'yes':
 
@@ -349,18 +315,19 @@ def main(argv=None):
         if not os.path.exists(s3_open_gene_accessibility_final_dir):
             os.makedirs(s3_open_gene_accessibility_final_dir)
 
-        input_gene_sparse_fl = args.gene_tn5_file
-        input_gene_bed_fl = args.gene_bed_file
+        ##updating 010425
+
+        #input_gene_sparse_fl = args.gene_tn5_file
+        #input_gene_bed_fl = args.gene_bed_file
 
         #input_gene_sparse_fl = output_dir + '/open_prepare_gene_tn5_final_dir/opt_gene_chnm.sparse'
         #input_gene_bed_fl = output_dir + '/open_prepare_gene_tn5_final_dir/opt_genes_500bpTSS_sorted.bed'
-        input_meta_fl = args.meta_file
+        #input_meta_fl = args.meta_file
 
         s3_geneAcc.calculate_gene_body_acc(input_required_scripts_dir,
-                                           input_gene_sparse_fl,
-                                           input_gene_bed_fl,
-                                           s3_open_gene_accessibility_final_dir,
-                                           input_meta_fl)
+                                           input_soc_obj_fl,
+                                           input_prefix,
+                                           s3_open_gene_accessibility_final_dir)
 
     if s4_open_smooth_gene_accessibility_final == 'yes':
 
@@ -370,21 +337,23 @@ def main(argv=None):
         if not os.path.exists(s4_open_smooth_gene_accessibility_final_dir):
             os.makedirs(s4_open_smooth_gene_accessibility_final_dir)
 
-        input_meta_fl = args.meta_file
-        input_svd_fl = args.svd_file
+        #input_meta_fl = args.meta_file
+        #input_svd_fl = args.svd_file
 
-        input_gene_accessibility_mtx_rds_fl = args.gene_accessibility_file
+        #input_gene_accessibility_mtx_rds_fl = args.gene_accessibility_file
 
         #input_gene_accessibility_mtx_rds_fl = output_dir + '/open_gene_accessibility_final_dir/opt_gene_body_accessibility_mtx.rd'
 
         s4_geneSmooth.smooth_gene_accessibility (input_required_scripts_dir,
-                                                 input_meta_fl,input_svd_fl,
-                                                 input_gene_accessibility_mtx_rds_fl,
+                                                 input_soc_obj_fl,
                                                  s4_open_smooth_gene_accessibility_final_dir,core_number_run,
-                                                 target_cluster_nm_final)
+                                                 target_cluster_nm_final,input_prefix)
 
-
-
+        ##remove the temp files
+        cmd = 'rm ' + s4_open_smooth_gene_accessibility_final_dir + '/*sparse' + ' ' + s4_open_smooth_gene_accessibility_final_dir + '/*.txt' + ' ' + \
+              s4_open_smooth_gene_accessibility_final_dir + '/all.normalizedActivity_mtx.rds'
+        print(cmd)
+        subprocess.call(cmd,shell=True)
 
 
 
