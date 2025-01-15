0aac563a6682f844045c03b6e41fe0dffea88b02
yanhaidong1 <35510112+yanhaidong1@users.noreply.github.com>
2025-01-15 15:43:41 +0800

    Add files via upload

--- utils/input_other_required_scripts_dir/utils_diff_peak_calling/s1_make_peak_sparse.py
+++ utils/input_other_required_scripts_dir/utils_diff_peak_calling/s1_make_peak_sparse.py
@@ -1,12 +1,5 @@
 #!/usr/bin/env python
 
-##updating 112222 we will build the data for the single organ
-##updating 062122 we will use all the peaks other than the organ specific peaks to find the right
-##updating 060222 set an option to call n binary peaks
-##updating 052422 build the sparse with ACR for each chromosome
-##updating 052322 we will make a combined version of sparse file
-##updating 052022 we will generate the peak information
-##updating 041522 we need to use the new peak file to generate the sparse file
 ##this script we will make the peak sparse file for each organ with using the parallele
 
 ##this script is to call peaks for each lib
@@ -14,37 +7,6 @@ import re
 import glob
 import subprocess
 import os
-
-
-
-#input_allorgans_peak_fl = sys.argv[1]
-##/scratch/hy17471/rice_altlas_scATAC_seq_042021/07_4_callpeaks_FDRway_pipelineVer_filterPeak_040722/pipeline_ver/opt_final_collect_organ_peaks_combineAll_052322/opt_final_peak_sorted.txt
-
-##/scratch/hy17471/rice_altlas_scATAC_seq_042021/07_4_callpeaks_FDRway_pipelineVer_filterPeak_040722/pipeline_ver/opt_final_collect_organ_peaks_052022
-
-##/scratch/hy17471/rice_altlas_scATAC_seq_042021/07_4_callpeaks_FDRway_pipelineVer_filterPeak_040722/pipeline_ver/output_dir_multipart/store_all_final_organ_flt_peaks_dir_sixpart_threepartBase
-
-##/scratch/hy17471/rice_altlas_scATAC_seq_042021/07_4_callpeaks_FDRway_pipelineVer_040322/output_dir_040322/opt_store_all_organs_unique_peak_dir/
-
-#input_tn5_bed_fl = sys.argv[2]
-#input_all_organ_tn5_dir = sys.argv[2]
-##/scratch/hy17471/rice_altlas_scATAC_seq_042021/01_post_process_build_meta_pipeline_062521_011322/collect_all_tn5_bed_dir_choose_for_sep_analysis/output_dir_combine_sort_ver
-
-#input_genome_fai_fl = sys.argv[3]
-##/scratch/hy17471/rice_altlas_scATAC_seq_042021/resources/Osativa323v7.fa.fai.sorted
-
-#input_config_fl = sys.argv[3]
-##
-
-#input_fastSparsetn5_pl = sys.argv[4]
-##fastSparse.tn5.pl
-
-##updating 060222
-##generate a non binary case
-#input_nfastSparsetn5_pl = sys.argv[5]
-
-#input_output_dir = sys.argv[5]
-
 
 
 
@@ -103,7 +65,73 @@ def sort_sparse (input_output_dir):
         subprocess.call(cmd,shell=True)
 
 
+def prepare_peak_acc (input_peak_tn5_fl,input_final_peak_fl,input_output_dir):
 
+    step01_prepare_peak_acc_dir = input_output_dir + '/opt_prepare_peak_acc_dir'
+    if not os.path.exists(step01_prepare_peak_acc_dir):
+        os.makedirs(step01_prepare_peak_acc_dir)
+
+
+
+    store_final_peak_dic = {}
+    with open (input_final_peak_fl,'r') as ipt:
+        for eachline in ipt:
+            eachline = eachline.strip('\n')
+            col = eachline.strip().split()
+            final_line = col[0] + '_' + col[1] + '_' + col[2]
+            store_final_peak_dic[final_line] = 1
+
+    store_all_acr_dic = {}
+    with open (input_peak_tn5_fl,'r') as ipt:
+        for eachline in ipt:
+            eachline = eachline.strip('\n')
+            col = eachline.strip().split()
+            new_peak = col[0].replace('chr','')
+            acrloc = '\t'.join(new_peak.split('_'))
+            store_all_acr_dic[acrloc] = new_peak
+
+    store_acr_ID_dic = {}
+    store_final_line_list = []
+    count = 0
+    for eachacrloc in store_all_acr_dic:
+
+        acrloc_nospace = store_all_acr_dic[eachacrloc]
+
+        count += 1
+        acrID = 'scACR_' + str(count)
+
+        if acrloc_nospace in store_final_peak_dic:
+
+            final_line = eachacrloc + '\t' + acrID + '\t' + '1'
+            store_final_line_list.append(final_line)
+
+            store_acr_ID_dic[acrloc_nospace] = acrID
+
+    with open (step01_prepare_peak_acc_dir + '/opt_peak.bed','w+') as opt:
+        for eachline in store_final_line_list:
+            opt.write(eachline + '\n')
+
+
+    tn5_fl = open (input_peak_tn5_fl,'r')
+    opt_fl = open (step01_prepare_peak_acc_dir + '/opt_accessibility.txt','w')
+
+    for eachline in tn5_fl:
+
+        eachline = eachline.strip('\n')
+        col = eachline.strip().split()
+
+        acrlocnospace = col[0]
+        acrlocnospace = acrlocnospace.replace('chr','')
+
+        if acrlocnospace in store_final_peak_dic:
+
+            acrID = store_acr_ID_dic[acrlocnospace]
+
+            final_line = acrID + '\t' + col[1] + '\t' + col[2]
+            opt_fl.write(final_line + '\n')
+
+    tn5_fl.close()
+    opt_fl.close()
 
 
 
