b017b862ec73e549b725e50a098f5960acde50f2
yanhaidong1 <35510112+yanhaidong1@users.noreply.github.com>
2025-06-30 21:21:09 +0800

    Add files via upload

--- utils/input_other_required_scripts_dir/utils_annotate_cell_identity_using_markers/plot_cell_identity_UMAP.R
+++ utils/input_other_required_scripts_dir/utils_annotate_cell_identity_using_markers/plot_cell_identity_UMAP.R
@@ -215,7 +215,7 @@ plotUMAP(b,ipt_output_dir,column='cell_identity', prefix=ipt_prefix,m1="umap1", 
 
 ##updating 062825
 ##write out the object
-final_meta_dt <- read.table(paste0(ipt_output_dir,'/',ipt_prefix,".annotated_cell_identity.meta.txt"))
+final_meta_dt <- read.delim(paste0(ipt_output_dir,'/',ipt_prefix,".annotated_cell_identity.meta.txt"),header = T,row.names = 1)
 final_obj <- append(ipt_soc_obj, list(
   final_meta = final_meta_dt
 ))
