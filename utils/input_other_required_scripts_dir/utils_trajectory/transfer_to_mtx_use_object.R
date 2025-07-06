aab294c3f9e7bc4750b009aea8f05a67a1ae8fa1
yanhaidong1 <35510112+yanhaidong1@users.noreply.github.com>
2025-07-06 13:24:05 +0800

    Add files via upload

---
+++ utils/input_other_required_scripts_dir/utils_trajectory/transfer_to_mtx_use_object.R
@@ -0,0 +1,27 @@
+###TopGO###
+library(Matrix)
+
+args <- commandArgs(T)
+
+# arguments
+input_object_fl <- as.character(args[1])
+
+#input_sparse_fl <- as.character(args[1])
+
+input_output_dir <- as.character(args[2])
+
+prefix <- as.character(args[3])
+
+
+
+#activity <- read.table(input_sparse_fl,stringsAsFactors = T)
+
+#activity <- sparseMatrix(i=as.numeric(activity$V1),
+#                         j=as.numeric(activity$V2),
+#                         x=as.numeric(activity$V3),
+#                         dimnames=list(levels(activity$V1), levels(activity$V2)))
+#atac_mtx <- as(activity, "dgCMatrix")
+input_object <- readRDS(input_object_fl)
+atac_mtx <- input_object$peak_tn5_mtx
+
+saveRDS(atac_mtx,paste0(input_output_dir,'/',prefix,'.rds'))
