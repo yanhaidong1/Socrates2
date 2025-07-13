998b48c806efe0c0d31ba84e1cb1defa909137b9
yanhaidong1 <35510112+yanhaidong1@users.noreply.github.com>
2025-07-13 21:00:35 +0800

    Add files via upload

--- utils/input_other_required_scripts_dir/utils_cell_type_peak_calling_dir_python/cleanBED.py
+++ utils/input_other_required_scripts_dir/utils_cell_type_peak_calling_dir_python/cleanBED.py
@@ -2,8 +2,13 @@
 
 import sys
 
+
+input_file = sys.argv[3]
+
 # Read chromosome lengths from the first input file
 chr_lengths = {}
+
+
 with open(sys.argv[1], 'r') as f:
     for line in f:
         parts = line.strip().split('\t')
@@ -14,22 +19,30 @@ factor = float(sys.argv[2]) / 1000000
 sys.stderr.write(f" - adjusted factor = {factor}\n")
 
 # Process the second file and apply adjustments
-with open(sys.argv[3], 'r') as f:
-    for line in f:
-        parts = line.strip().split('\t')
-        chrom = parts[0]
-        start = int(parts[1])
-        end = int(parts[2])
-        value = float(parts[3])
 
-        # Adjust end if it exceeds chromosome length
-        if end > chr_lengths.get(chrom, 0):
-            end = chr_lengths[chrom] - 1
-            if (end - start) < 1:
-                continue
 
-        if start >= chr_lengths.get(chrom, 0):
+if input_file == '-':
+    f = sys.stdin
+else:
+    f = open(input_file, 'r')
+
+
+#with open(sys.stdin, 'r') as f:
+for line in f:
+    parts = line.strip().split('\t')
+    chrom = parts[0]
+    start = int(parts[1])
+    end = int(parts[2])
+    value = float(parts[3])
+
+    # Adjust end if it exceeds chromosome length
+    if end > chr_lengths.get(chrom, 0):
+        end = chr_lengths[chrom] - 1
+        if (end - start) < 1:
             continue
 
-        adjusted_value = value / factor
-        print(f"{chrom}\t{start}\t{end}\t{adjusted_value}")
+    if start >= chr_lengths.get(chrom, 0):
+        continue
+
+    adjusted_value = value / factor
+    print(f"{chrom}\t{start}\t{end}\t{adjusted_value}")
