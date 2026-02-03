ea3587202ba3e70a392c77f76e48184d39fdaff0
yanhaidong1 <35510112+yanhaidong1@users.noreply.github.com>
2026-02-03 14:39:21 +0800

    Add files via upload

--- utils/input_other_required_scripts_dir/utils_load_and_QC_data/load_and_QC.R
+++ utils/input_other_required_scripts_dir/utils_load_and_QC_data/load_and_QC.R
@@ -1,5 +1,6 @@
 # run Socrates on merged socrates object #
 
+##updating 020326 updating if no iscell, we will use the find cell to save the cells
 ##updating 111525 we will set an option if no reads in the Pt OR Mt we will not filter the organell
 ##updating 020125 add the max reads
 ##updating 100821 we will do Socrates from very beginning
@@ -34,209 +35,232 @@ load_all(path_to_preload_R_script)
 ##load all the parameters
 source(config)
 
-##set the condition to run each step
-if (open_build_object_final == 'yes'){
 
-  ##load the obj
-  obj <- loadBEDandGenomeData(bed, ann, chr)
-  ##count organellar reads
-  obj <- countRemoveOrganelle(obj, org_scaffolds=c(Pt, Mt), remove_reads=T)  ##ChrPt and ChrMt for the rice
-  ##call peaks 
-  obj <- callACRs(obj, genomesize=genomesize, 
-                  shift= macs2_shift_final, 
-                  extsize= macs2_extsize_final,
-                  fdr= macs2_fdr_final,
-                  output=paste0(out,"_peaks"), 
-                  tempdir=paste0(output_dir,'/',out, '_macs2_temp'), 
-                  verbose=T)
-  ##build meta data
-  obj <- buildMetaData(obj,
-                       tss.window= tss_window_size_final, 
-                       verbose=TRUE)
-  saveRDS(obj,paste0(output_dir,'/',out,'.raw_obj.rds'))
-  meta_fl <-obj$meta
-  write.table(meta_fl, file=paste0(output_dir,'/',out, ".raw_obj.metadata.txt"), quote=F, row.names=T, col.names=T, sep="\t")
-}
-
-##updating 111125
-##we will check if the raw meta file contain the organel
-total_ptmt <- sum(obj$ptmt)
-if (total_ptmt == 0){
-  message(" - We did not identify organelle reads and therefore will not perform the filtration step. ...")
-  filt.org_val = F
+if (open_build_object_final == 'no' & open_find_cells_step_final == 'no' & open_is_cells_step_final == 'no'){
+  obj_find_cell <- readRDS(paste0(output_dir,'/',out,'.findcell_obj.rds'))
+  soc.obj <- convertSparseData(obj_find_cell, verbose=T)
+  saveRDS(soc.obj, file=paste0(output_dir,'/',out,".filtered.soc.rds"))
 }else{
-  filt.org_val = T
-}
 
-if (open_find_cells_step_final == 'yes'){
-  
+  ##set the condition to run each step
   if (open_build_object_final == 'yes'){
-
-    message(" - finding cells ...")
-    
-    ##udpating 031925
-    if (open_knee_plot_filter_cell == 'yes'){
-      obj <- findCells(obj, 
-                       output_dir,
-                       doplot=T,
-                       min.cells= min_cell_val_final,
-                       max.cells= max_cell_val_final,
-                       set.tn5.cutoff=NULL,
-                       min.tn5=min_tn5_val,
-                       max.tn5=max_tn5_val,
-                       org.filter.thresh=organelle_filter_cutoff_final,
-                       tss.min.freq=tss_min_freq_val_final,
-                       tss.z.thresh=tss_z_thresh_final,
-                       frip.min.freq=frip_min_freq_final,
-                       frip.z.thresh=frip_z_thresh_final,
-                       prefix=out,
-                       filt.org = filt.org_val)
-    }else{
-      message('users not choose to perform the knee plot')
-      obj <- findCells_no_knee_plot_filter(obj, 
-                       output_dir,
-                       doplot=T,
-                       min.cells= min_cell_val_final,
-                       max.cells= max_cell_val_final,
-                       set.tn5.cutoff=NULL,
-                       min.tn5=min_tn5_val,
-                       max.tn5=max_tn5_val,
-                       org.filter.thresh=organelle_filter_cutoff_final,
-                       tss.min.freq=tss_min_freq_val_final,
-                       tss.z.thresh=tss_z_thresh_final,
-                       frip.min.freq=frip_min_freq_final,
-                       frip.z.thresh=frip_z_thresh_final,
-                       prefix=out,
-                       filt.org = filt.org_val)
-      
-    }
-    
-    
-    
-    obj <- generateMatrix(obj,
-                          filtered=T, 
-                          windows=win_size, 
-                          peaks=F, 
-                          verbose=T)
-  }else{
-    
-    message(" - finding cells ...")
-    message(" - load the raw object")
-    obj <- readRDS(paste0(output_dir,'/',out,'.raw_obj.rds'))
-    #obj <- findCells(obj, 
-    #                 output_dir,
-    #                 doplot=T,
-    #                 max.cells=16000,
-    #                 set.tn5.cutoff=1000,
-    #                 frip.min.freq=0.2,
-    #                 frip.z.thresh=1,
-    #                 prefix=out)
-    
-    ##udpating 031925
-    if (open_knee_plot_filter_cell == 'yes'){
-      
-      obj <- findCells(obj, 
-                       output_dir,
-                       doplot=T,
-                       min.cells= min_cell_val_final,
-                       max.cells= max_cell_val_final,
-                       set.tn5.cutoff=NULL,
-                       min.tn5=min_tn5_val,
-                       max.tn5=max_tn5_val,
-                       org.filter.thresh=organelle_filter_cutoff_final,
-                       tss.min.freq=tss_min_freq_val_final,
-                       tss.z.thresh=tss_z_thresh_final,
-                       frip.min.freq=frip_min_freq_final,
-                       frip.z.thresh=frip_z_thresh_final,
-                       prefix=out,
-                       filt.org = filt.org_val)
-    }else{
-      message('users not choose to perform the knee plot')
-      obj <- findCells_no_knee_plot_filter(obj, 
-                       output_dir,
-                       doplot=T,
-                       min.cells= min_cell_val_final,
-                       max.cells= max_cell_val_final,
-                       set.tn5.cutoff=NULL,
-                       min.tn5=min_tn5_val,
-                       max.tn5=max_tn5_val,
-                       org.filter.thresh=organelle_filter_cutoff_final,
-                       tss.min.freq=tss_min_freq_val_final,
-                       tss.z.thresh=tss_z_thresh_final,
-                       frip.min.freq=frip_min_freq_final,
-                       frip.z.thresh=frip_z_thresh_final,
-                       prefix=out,
-                       filt.org = filt.org_val)
-      
-    }
-    
-    
-    
-    obj <- generateMatrix(obj,
-                          filtered=T, 
-                          windows=win_size, 
-                          peaks=F, 
-                          verbose=T)
+  
+    ##load the obj
+    obj <- loadBEDandGenomeData(bed, ann, chr)
+    ##count organellar reads
+    obj <- countRemoveOrganelle(obj, org_scaffolds=c(Pt, Mt), remove_reads=T)  ##ChrPt and ChrMt for the rice
+    ##call peaks 
+    obj <- callACRs(obj, genomesize=genomesize, 
+                    shift= macs2_shift_final, 
+                    extsize= macs2_extsize_final,
+                    fdr= macs2_fdr_final,
+                    output=paste0(out,"_peaks"), 
+                    tempdir=paste0(output_dir,'/',out, '_macs2_temp'), 
+                    verbose=T)
+    ##build meta data
+    obj <- buildMetaData(obj,
+                         tss.window= tss_window_size_final, 
+                         verbose=TRUE)
+    saveRDS(obj,paste0(output_dir,'/',out,'.raw_obj.rds'))
+    meta_fl <-obj$meta
+    write.table(meta_fl, file=paste0(output_dir,'/',out, ".raw_obj.metadata.txt"), quote=F, row.names=T, col.names=T, sep="\t")
   }
   
-  saveRDS(obj,paste0(output_dir,'/',out,'.findcell_obj.rds'))
-  
-}
-
-if (open_is_cells_step_final == 'yes'){
+  ##updating 111125
+  ##we will check if the raw meta file contain the organel
+  total_ptmt <- sum(obj$ptmt)
+  if (total_ptmt == 0){
+    message(" - We did not identify organelle reads and therefore will not perform the filtration step. ...")
+    filt.org_val = F
+  }else{
+    filt.org_val = T
+  }
   
   if (open_find_cells_step_final == 'yes'){
     
-    message(" - filter cell by comparing background ...")
-    obj <- isCell(obj,output_dir,
-                  num.test=num_test_val_final,
-                  num.tn5=num_tn5_val_final,
-                  num.ref=num_ref_val_final,
-                  background.cutoff=min_tn5_val+1000,
-                  min.pTSS=0.2,
-                  min.FRiP=0.2,
-                  min.pTSS.z= -2,
-                  min.FRiP.z= -2,
-                  verbose=F,
-                  prefix=out
-                  )
-
+    if (open_build_object_final == 'yes'){
+  
+      message(" - finding cells ...")
+      
+      ##udpating 031925
+      if (open_knee_plot_filter_cell == 'yes'){
+        obj <- findCells(obj, 
+                         output_dir,
+                         doplot=T,
+                         min.cells= min_cell_val_final,
+                         max.cells= max_cell_val_final,
+                         set.tn5.cutoff=NULL,
+                         min.tn5=min_tn5_val,
+                         max.tn5=max_tn5_val,
+                         org.filter.thresh=organelle_filter_cutoff_final,
+                         tss.min.freq=tss_min_freq_val_final,
+                         tss.z.thresh=tss_z_thresh_final,
+                         frip.min.freq=frip_min_freq_final,
+                         frip.z.thresh=frip_z_thresh_final,
+                         prefix=out,
+                         filt.org = filt.org_val)
+      }else{
+        message('users not choose to perform the knee plot')
+        obj <- findCells_no_knee_plot_filter(obj, 
+                         output_dir,
+                         doplot=T,
+                         min.cells= min_cell_val_final,
+                         max.cells= max_cell_val_final,
+                         set.tn5.cutoff=NULL,
+                         min.tn5=min_tn5_val,
+                         max.tn5=max_tn5_val,
+                         org.filter.thresh=organelle_filter_cutoff_final,
+                         tss.min.freq=tss_min_freq_val_final,
+                         tss.z.thresh=tss_z_thresh_final,
+                         frip.min.freq=frip_min_freq_final,
+                         frip.z.thresh=frip_z_thresh_final,
+                         prefix=out,
+                         filt.org = filt.org_val)
+        
+      }
+      
+      
+      
+      obj <- generateMatrix(obj,
+                            filtered=T, 
+                            windows=win_size, 
+                            peaks=F, 
+                            verbose=T)
+    }else{
+      
+      message(" - finding cells ...")
+      message(" - load the raw object")
+      obj <- readRDS(paste0(output_dir,'/',out,'.raw_obj.rds'))
+      #obj <- findCells(obj, 
+      #                 output_dir,
+      #                 doplot=T,
+      #                 max.cells=16000,
+      #                 set.tn5.cutoff=1000,
+      #                 frip.min.freq=0.2,
+      #                 frip.z.thresh=1,
+      #                 prefix=out)
+      
+      ##udpating 031925
+      if (open_knee_plot_filter_cell == 'yes'){
+        
+        obj <- findCells(obj, 
+                         output_dir,
+                         doplot=T,
+                         min.cells= min_cell_val_final,
+                         max.cells= max_cell_val_final,
+                         set.tn5.cutoff=NULL,
+                         min.tn5=min_tn5_val,
+                         max.tn5=max_tn5_val,
+                         org.filter.thresh=organelle_filter_cutoff_final,
+                         tss.min.freq=tss_min_freq_val_final,
+                         tss.z.thresh=tss_z_thresh_final,
+                         frip.min.freq=frip_min_freq_final,
+                         frip.z.thresh=frip_z_thresh_final,
+                         prefix=out,
+                         filt.org = filt.org_val)
+      }else{
+        message('users not choose to perform the knee plot')
+        obj <- findCells_no_knee_plot_filter(obj, 
+                         output_dir,
+                         doplot=T,
+                         min.cells= min_cell_val_final,
+                         max.cells= max_cell_val_final,
+                         set.tn5.cutoff=NULL,
+                         min.tn5=min_tn5_val,
+                         max.tn5=max_tn5_val,
+                         org.filter.thresh=organelle_filter_cutoff_final,
+                         tss.min.freq=tss_min_freq_val_final,
+                         tss.z.thresh=tss_z_thresh_final,
+                         frip.min.freq=frip_min_freq_final,
+                         frip.z.thresh=frip_z_thresh_final,
+                         prefix=out,
+                         filt.org = filt.org_val)
+        
+      }
+      
+      
+      
+      obj <- generateMatrix(obj,
+                            filtered=T, 
+                            windows=win_size, 
+                            peaks=F, 
+                            verbose=T)
+    }
+    
+    saveRDS(obj,paste0(output_dir,'/',out,'.findcell_obj.rds'))
+    
+  }
+  
+  if (open_is_cells_step_final == 'yes'){
+    
+    if (open_find_cells_step_final == 'yes'){
+      
+      message(" - filter cell by comparing background ...")
+      obj <- isCell(obj,output_dir,
+                    num.test=num_test_val_final,
+                    num.tn5=num_tn5_val_final,
+                    num.ref=num_ref_val_final,
+                    background.cutoff=min_tn5_val+1000,
+                    min.pTSS=0.2,
+                    min.FRiP=0.2,
+                    min.pTSS.z= -2,
+                    min.FRiP.z= -2,
+                    verbose=F,
+                    prefix=out
+                    )
+  
+    }else{
+      
+      message(" - filter cell by comparing background ...")
+      message(" - laod the findcell object ...")
+      obj <- readRDS(paste0(output_dir,'/',out,'.findcell_obj.rds'))
+      obj <- isCell(obj,output_dir,
+                    num.test=num_test_val_final,
+                    num.tn5=num_tn5_val_final,
+                    num.ref=num_ref_val_final,
+                    background.cutoff=min_tn5_val+1000,
+                    min.pTSS=0.2,
+                    min.FRiP=0.2,
+                    min.pTSS.z= -2,
+                    min.FRiP.z= -2,
+                    verbose=F,
+                    prefix=out
+                    )
+      
+      
+    }
   }else{
     
-    message(" - filter cell by comparing background ...")
-    message(" - laod the findcell object ...")
+    message(" - close the isCell")
     obj <- readRDS(paste0(output_dir,'/',out,'.findcell_obj.rds'))
-    obj <- isCell(obj,output_dir,
-                  num.test=num_test_val_final,
-                  num.tn5=num_tn5_val_final,
-                  num.ref=num_ref_val_final,
-                  background.cutoff=min_tn5_val+1000,
-                  min.pTSS=0.2,
-                  min.FRiP=0.2,
-                  min.pTSS.z= -2,
-                  min.FRiP.z= -2,
-                  verbose=F,
-                  prefix=out
-                  )
     
     
   }
-}else{
-  
-  message(" - close the isCell")
-  obj <- readRDS(paste0(output_dir,'/',out,'.findcell_obj.rds'))
   
   
+  
+  
+  
+  
+  
+  
+  # save QC data
+  saveRDS(obj, file=paste0(output_dir,'/',out, ".QC.rds"))
+  # convert to Socrates format for downstream analysis. 
+  soc.obj <- convertSparseData(obj, verbose=T)
+  
+  ##check if the soc.obj contains the counts
+  if (dim(soc.obj$counts)[2] == 0){
+    obj_find_cell <- readRDS(paste0(output_dir,'/',out,'.findcell_obj.rds'))
+    soc.obj <- convertSparseData(obj_find_cell, verbose=T)
+  }
+  
+  # save QC object
+  saveRDS(soc.obj, file=paste0(output_dir,'/',out,".filtered.soc.rds"))
+
 }
 
 
-
-# save QC data
-saveRDS(obj, file=paste0(output_dir,'/',out, ".QC.rds"))
-# convert to Socrates format for downstream analysis. 
-soc.obj <- convertSparseData(obj, verbose=T)
-# save QC object
-saveRDS(soc.obj, file=paste0(output_dir,'/',out,".filtered.soc.rds"))
 
 
 
